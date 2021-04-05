ea236293f12a080581d587ee57e8e6fd399ddf3f
Daniel Medina <daniel@medina.com>
2021-04-04 21:56:30 -0500

    again urls

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -31,18 +31,18 @@ export class ProjectService {
 
     getProject(id): Observable<any> {
         let headers = new HttpHeaders().set('Content-Type', 'application/json');
-        return this._http.get(`${this.url}project/${id}`);
+        return this._http.get(`${this.url}/project/${id}`);
     }
 
     deleteProject(id): Observable<any> {
         let headers = new HttpHeaders().set('Content-Type', 'application/json');
-        return this._http.delete(this.url + 'project/' + id, { headers: headers });
+        return this._http.delete(this.url + '/project/' + id, { headers: headers });
     }
 
     updateProject(project): Observable<any> {
         let params = JSON.stringify(project);
         let headers = new HttpHeaders().set('Content-Type', 'application/json');
-        return this._http.put(this.url + 'project/' + project._id, params, { headers: headers });
+        return this._http.put(this.url + '/project/' + project._id, params, { headers: headers });
     }
 
 }
